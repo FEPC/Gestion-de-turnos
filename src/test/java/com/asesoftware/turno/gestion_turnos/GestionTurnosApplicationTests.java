546000ce506fcd228aa9c427cde7a4bc23b5bb48
fpeña <fpena@asesoftware.com>
2021-06-07 14:24:48 -0500

    Primer commit

---
+++ src/test/java/com/asesoftware/turno/gestion_turnos/GestionTurnosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.asesoftware.turno.gestion_turnos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionTurnosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
